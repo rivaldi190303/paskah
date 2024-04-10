485a152a480930d2d70c84350c8d47afba086aef
rivaldi190303 <113177773+rivaldi190303@users.noreply.github.com>
2024-04-10 17:02:08 +0700

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@
       // Menambahkan data baru ke array
       existingData.push(data);
 
-      // Menyimpan array data kembali ke localStorage
+     
       localStorage.setItem('data', JSON.stringify(existingData));
 
       alert('Data berhasil disimpan!');
@@ -26,12 +26,7 @@
       
     ;
 
-  // You can perform further actions with the form data here,
-  // such as sending it to a server using AJAX or processing it locally
 
-  // For this example, let's just log the form data to the console
-
-// Function to display thank you message
 function showThankYou() {
   // Prevent form submission
   event.preventDefault();
@@ -47,16 +42,12 @@ function showThankYou() {
     }
   }
 function showThankYou() {
-    // Sematkan pesan terima kasih
     var thankYouDiv = document.getElementById("thankYou");
     thankYouDiv.style.display = "block";
 var waDiv = document.getElementById("wa");
   waDiv.style.display = "block";
-    // Sembunyikan formulir
     var formDiv = document.getElementById("registrationForm");
     formDiv.style.display = "none";
-
-    // Sembunyikan gambar angka dua
     var img2Div = document.getElementById("img2");
     img2Div.style.display = "none";
 
@@ -64,14 +55,12 @@ var waDiv = document.getElementById("wa");
     var submitButtonDiv = document.getElementById("submitbutton");
     submitButtonDiv.style.display = "none";
   }
-  // Show thank you message
+  
 document.getElementById("persetujuan").style.display= "none"; document.getElementById('thankYou').style.display = 'block';
 }
 
-// Function to close window
 function closeWindow() {
   // Close window
   window.close();
 }
-// Add event listener to form submission
 document.getElementById('registrationForm').addEventListener('submit', submitForm);
